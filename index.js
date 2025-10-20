ecf8ebb7f5e7595514875216502d131539e8a75f
wionquintela <wionquintela06@gmail.com>
2025-10-21 01:03:34 +0800

    created repo

---
+++ index.js
@@ -0,0 +1,5 @@
+function validateForm() {
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+  const verifiedID = document.getElementById('verifiedID').value;
+}
