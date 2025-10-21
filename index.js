78fb220d884247468e1b84b5f09844766f99d714
wionquintela <wionquintela06@gmail.com>
2025-10-21 11:42:27 +0800

    added main menu and authentication

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 function validateForm() {
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
-  const verifiedID = document.getElementById('verifiedID').value;
+  const form = document.getElementById("authForm");
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const userName = form.userName.value.trim();
+    const password = form.password.value.trim();
+    const verifiedID = form.verifiedID.value.trim();
+
+    if(userName == "admin" && password == "adminPass" && verifiedID == "0001-A") {
+      alert(`Welcome, admin ${userName}`);
+      window.location.href = "mainMenu.html";
+    } else if (userName == "staff" && password == "staffPass" && verifiedID == "0001-S") {
+      alert(`welcome, staff ${userName}`);
+      window.location.href = "mainMenu.html";
+    } else if (userName == "doctor" && password == "doctorPass" && verifiedID == "0001-D") {
+      alert( `Welcome, doctor ${doctor}`);
+      window.location.href = "mainMenu.html";
+    } else {
+      alert("Sorry the user does not exist");
+    }
+  })
 }
+
+validateForm();
+
+document.getElementById("userGreet").innerHTML = `Welcome, ${userName}`
